f1cdb5187e90ef08c16cdb68f0adf0fd336dedea
ywhhui <ywh_living@163.com>
2023-05-10 14:38:53 +0800

    # ECB 简单快速但安全性较差，适用于短数据的加密和不需要保证机密性的场景；CBC 安全性较高但无法并行加密，且需要进行填充操作。在实际应用中，我们可以根据需要选择合适的加密模式。

--- src/main/java/com/szcgc/cougua/utils/AesThreadSafeUtils.java
+++ src/main/java/com/szcgc/cougua/utils/AesThreadSafeUtils.java
@@ -3,6 +3,7 @@ package com.szcgc.cougua.utils;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -10,57 +11,105 @@ import javax.crypto.spec.SecretKeySpec;
  */
 public class AesThreadSafeUtils {
     private static final String ALGORITHM = "AES";
-    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+    private static final String TRANSFORMATION_ECB = "AES/ECB/PKCS5Padding";
+    private static final String TRANSFORMATION_CBC = "AES/CBC/PKCS5Padding";
     private static ThreadLocal<SecretKeySpec> threadLocalKeySpec = new ThreadLocal<>();
-    private static ThreadLocal<Cipher> threadLocalCipher = new ThreadLocal<>();
+    private static ThreadLocal<Cipher> threadLocalCipher_ECB = new ThreadLocal<>();
+    private static ThreadLocal<Cipher> threadLocalCipher_CBC = new ThreadLocal<>();
     //偏移量,AES 为16bytes. DES 为8bytes
     private static final String IV = "1234567890123456";
-
+    private static final String CHARSET = "UTF-8";
     /**
-     * AES加密
+     * ECB AES加密
      *
      * @param input 将要加密的内容
      * @param key 密钥
      * @return 已经加密的字节数组内容 再 base64 之后的字符串
      */
-    public static String encrypt(byte[] input, byte[] key) throws Exception {
+    public static String encryptECB(byte[] input, byte[] key) throws Exception {
         SecretKeySpec keySpec = threadLocalKeySpec.get();
         if (keySpec == null) {
             keySpec = new SecretKeySpec(key, ALGORITHM);
             threadLocalKeySpec.set(keySpec);
         }
-        Cipher cipher = threadLocalCipher.get();
+        Cipher cipher = threadLocalCipher_ECB.get();
         if (cipher == null) {
-            cipher = Cipher.getInstance(TRANSFORMATION);
-            threadLocalCipher.set(cipher);
+            cipher = Cipher.getInstance(TRANSFORMATION_ECB);
+            threadLocalCipher_ECB.set(cipher);
         }
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         return Base64.encodeBase64String(cipher.doFinal(input));
     }
 
     /**
-     *
+     *ECB AES解密
      * @param input
      * @param key
      * @return
      * @throws Exception
      */
-    public static String decrypt(String input, byte[] key) throws Exception {
+    public static String decryptECB(String input, byte[] key) throws Exception {
         SecretKeySpec keySpec = threadLocalKeySpec.get();
         if (keySpec == null) {
             keySpec = new SecretKeySpec(key, ALGORITHM);
             threadLocalKeySpec.set(keySpec);
         }
-        Cipher cipher = threadLocalCipher.get();
+        Cipher cipher = threadLocalCipher_ECB.get();
         if (cipher == null) {
-            cipher = Cipher.getInstance(TRANSFORMATION);
-            threadLocalCipher.set(cipher);
+            cipher = Cipher.getInstance(TRANSFORMATION_ECB);
+            threadLocalCipher_ECB.set(cipher);
         }
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         byte[] result = cipher.doFinal(Base64.decodeBase64(input));
-        return new String(result,"utf-8");
+        return new String(result,CHARSET);
     }
 
+    /**
+     * CBC AES加密
+     *
+     * @param input 将要加密的内容
+     * @param key 密钥
+     * @return 已经加密的字节数组内容 再 base64 之后的字符串
+     */
+    public static String encryptCBC(byte[] input, byte[] key) throws Exception {
+        SecretKeySpec keySpec = threadLocalKeySpec.get();
+        if (keySpec == null) {
+            keySpec = new SecretKeySpec(key, ALGORITHM);
+            threadLocalKeySpec.set(keySpec);
+        }
+        Cipher cipher = threadLocalCipher_CBC.get();
+        if (cipher == null) {
+            cipher = Cipher.getInstance(TRANSFORMATION_CBC);
+            threadLocalCipher_CBC.set(cipher);
+        }
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(IV.getBytes(CHARSET));
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec,ivParameterSpec);
+        return Base64.encodeBase64String(cipher.doFinal(input));
+    }
+
+    /**
+     *CBC AES解密
+     * @param input
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static String decryptCBC(String input, byte[] key) throws Exception {
+        SecretKeySpec keySpec = threadLocalKeySpec.get();
+        if (keySpec == null) {
+            keySpec = new SecretKeySpec(key, ALGORITHM);
+            threadLocalKeySpec.set(keySpec);
+        }
+        Cipher cipher = threadLocalCipher_CBC.get();
+        if (cipher == null) {
+            cipher = Cipher.getInstance(TRANSFORMATION_CBC);
+            threadLocalCipher_CBC.set(cipher);
+        }
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(IV.getBytes(CHARSET));
+        cipher.init(Cipher.DECRYPT_MODE, keySpec,ivParameterSpec);
+        byte[] result = cipher.doFinal(Base64.decodeBase64(input));
+        return new String(result,"utf-8");
+    }
 
     public static void main(String[] args) {
         try {
@@ -69,10 +118,15 @@ public class AesThreadSafeUtils {
             //AES/ECB/NoPadding填充模式
             String ecbEn = null;
             try {
-                ecbEn = encrypt(str.getBytes("UTF-8"), key.getBytes("UTF-8"));
+                ecbEn = encryptECB(str.getBytes("UTF-8"), key.getBytes(CHARSET));
                 System.out.println("ecb加密后"+ecbEn);
-                String ecbDe = decrypt(ecbEn, key.getBytes("UTF-8"));
+                String ecbDe = decryptECB(ecbEn, key.getBytes(CHARSET));
                 System.out.println("ecbDe解密后"+ecbDe);
+
+                ecbEn = encryptCBC(str.getBytes("UTF-8"), key.getBytes(CHARSET));
+                System.out.println("ecb加密后"+ecbEn);
+                String ecbDe2 = decryptCBC(ecbEn, key.getBytes(CHARSET));
+                System.out.println("ecbDe解密后"+ecbDe2);
             } catch (Exception e) {
                 e.printStackTrace();
             }
